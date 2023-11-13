173693d4ff94a50d257ad767a4d16ac1cc60f3e3
Nicoly <nicolyvalferreira.564@gmail.com>
2023-11-13 17:14:46 -0300

    feat: controoll

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -1,27 +1,45 @@
+import { Student } from "../models/students/Student.js";
+import { StudentList } from "../models/students/StudentsList.js";
+
+const list = new StudentList();
 
 export const getStudents = (req, res) => {
+    const students = list.getAllStudents();
+    if (students.length) {
+        return res.status(404).send(
+            students);
+    }
     return res.status(200).send(
-        { message: "Get all students controller!" });
+        { message: "Não há alunos cadastrados" });
 };
+
 export const getStudentById = (req, res) => {
+
     const { id } = req.params;
+    const student = list.getStudentById(id);
+    if (!student) {
+        return res.status(404).send(
+            { message: `Student with id ${id} not found!` }
+        );
+    }
     return res.status(200).send(
-        { message: `Get student with id ${id} controller!` }
+        student
     );
 };
+
 export const createStudent = (req, res) => {
     const { name, email, age } = req.body;
-    age >= 18 ? res.status(201).send({ message: "Aluno com idade maior ou igual a 18 anos" })
-        : res.status(400).send({ message: "Aluno com idade menor que 18 anos" });
     if (!name || !email || !age) {
         return res.status(400).send(
-            { message: "Missing required informations!" }
+            { message: "Parâmetros inválidos!" }
         );
-    };
-    return res.status(200).send(
-        { message: `Created student with name ${name}, email: ${email} and age ${age}!` }
-    );
+    }
+
+    const student = new Student(name, email, age);
+    list.addStudent(student);
+    return res.status(201).send({ message: "Aluno criado com sucesso!", student, });
 }
+
 export const updateStudent = (req, res) => {
     const { id } = req.params;
     const { name, email, age } = req.body;
@@ -29,15 +47,29 @@ export const updateStudent = (req, res) => {
         return res.status(400).send(
             { message: "Missing required informations!" }
         );
-    };
-    return res.status(200).send({
-        message: `Updated student with id ${id} with name ${name}, that is ${age} years old and email: ${email}`
-    });
+    }
+    const student = list.updateStudent(id, name, email, age);
+    if (!student) {
+        return res.status(404).send(
+            { message: `Student with id ${id} not found!` }
+        );
+    }
+
+    const updatedStudent = list.updateStudent(id, name, email, age);
+    return res.status(200).send({ message: "Student updated successfully!", updatedStudent });
 };
 
 export const deleteStudent = (req, res) => {
     const { id } = req.params;
+    const student = list.getStudentById(id);
+    if (!student) {
+        return res.status(404).send(
+            { message: `Student not found!` }
+        );
+    }
+    list.removeStudent(id);
+
     return res.status(200).send(
-        { message: `Deleted student with id ${id}!` }
+        { message: "Aluno encontrado com sucesso!", student, }
     );
 }
