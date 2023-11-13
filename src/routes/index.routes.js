e60ea48372314741e0257156ca375998093b6a5e
Nicoly <nicolyvalferreira.564@gmail.com>
2023-11-13 14:51:01 -0300

    feat: add course route in index

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import studentsRoutes from './students.routes.js';
+import coursesRoutes from './course.routes.js';
 
 const rotas = Router();
 rotas.use("/students", studentsRoutes);
+rotas.use("/courses", coursesRoutes);
 
 rotas.get("/", (req, res) => {
     return res.status(200).send(
