1d1706e2218ef26429cb410522fd5022d709444e
Nicoly <nicolyvalferreira.564@gmail.com>
2023-11-13 09:36:40 -0300

    feat:  create a new route

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,4 +2,10 @@ import {Router} from 'express';
 
 const rotas = Router();
 
+rotas.get("/", (req, res) => {
+    return res.status(200).send(
+        {message:"servidor ok!"}
+    )
+});
+
 export default rotas;
