31c595b6a5cd17f1c7256245f96fc5d0862d7891
Nicoly <nicolyvalferreira.564@gmail.com>
2023-11-13 10:27:09 -0300

    feat: import routes from students

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,8 @@
 import {Router} from 'express';
+import studentsRoutes from './students.routes.js';
 
 const rotas = Router();
+rotas.use("/students", studentsRoutes);
 
 rotas.get("/", (req, res) => {
     return res.status(200).send(
